a305bd0a835a3bd889c0b4c55fcb3fc1dc40ba54
BojidarPanaiotov <cccpisthebest@abv.bg>
2021-07-24 17:19:08 +0300

    New project created

---
+++ Criminal-Web-Station/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Criminal_Web_Station.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
